a847307604824a18f225f0b4a7159792a72edece
Precious Marquez <precious.marquez@adish.com.ph>
2025-03-24 02:27:03 +0000

    feat(users): implement add, edit, delete, and reset password functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,42 @@
 class User < ApplicationRecord
+  attr_accessor :remove_profile
+  has_one_attached :profile_picture
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: { case_sensitive: true }, format: { 
+    with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]{3,}\z/i, message: "must be a valid email address" }
+  
+  validates :employment_status, :gender, presence: true
+  validates :f_name, presence: true, length: { minimum: 2 }, if: -> { f_name.present? }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :m_name, presence: true, length: { minimum: 2 }, if: -> { m_name.present? }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :l_name, presence: true, length: { minimum: 2 }, if: -> { l_name.present? }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :job_title, presence: true, length: { minimum: 2 }, if: -> { job_title.present? }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :employee_id, presence: true, numericality: { only_integer: true }, format: { with: /\A\d+\z/, message: "only allows numbers" }
+  validates :employee_id, uniqueness: true
+  validates :salary, presence: true, length: { minimum: 4 }, numericality: { only_integer: true }, format: { with: /\A\d+\z/, message: "only allows numbers" }
+  validates :password, format: { with: /\A(?=.*[A-Z])(?=.*\d)(?=.*[a-zA-Z]).{6,}\z/, message: "must contain at least one uppercase letter, one number, and be at least 6 characters long" }
+
+  validate :age_is_18_above
+  validate :legal_age
+
+
+  private
+  def age_is_18_above
+    if birthday.present? && birthday > 18.years.ago.to_date
+      errors.add(:birthday, "must be at least 18 years old")
+    end
+  end
+
+  def legal_age
+    if birthday.present? && hire_date.present?
+      if birthday > hire_date.years_ago(18)
+        errors.add(:hire_date, "must be at least 18 years old to work")
+      end
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_18_024517) do
+ActiveRecord::Schema.define(version: 2025_03_22_032329) do
+
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "roles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -55,6 +76,8 @@ ActiveRecord::Schema.define(version: 2025_03_18_024517) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "net_salary", precision: 10
+    t.string "temporary_password"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["employee_id"], name: "index_users_on_employee_id", unique: true
@@ -70,4 +93,5 @@ ActiveRecord::Schema.define(version: 2025_03_18_024517) do
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
