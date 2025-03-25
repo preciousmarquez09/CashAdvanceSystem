81c40bdd7850b007892fe8c0989555d3179eb1a6
JohnPaul <john.estanislao@adish.com.ph>
2025-03-25 07:46:30 +0000

    feat(admin): add dashboard with pending, approved, ongoing request stats and audit logs using Tailwind CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+
   namespace :admin do
     resources :users do
       post :reset_password, on: :member
     end
+
+    get "dashboard", to: "dashboard#index"
   end
-  
-  
 end
