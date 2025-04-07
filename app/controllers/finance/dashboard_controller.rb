496c0900bbd06aa1811d15cf807796cb341d670a
JohnPaul <john.estanislao@adish.com.ph>
2025-04-07 09:16:28 +0000

    feat(dashboard): add due to next payroll table and my account section

--- app/controllers/finance/dashboard_controller.rb
+++ app/controllers/finance/dashboard_controller.rb
@@ -1,13 +1,24 @@
 
 class Finance::DashboardController < ApplicationController
-    
+  include Pagy::Backend
   
-    def index
-      @total_pending_requests = CashAdvRequest.where(status: 'pending').count
-      @total_approved_requests = CashAdvRequest.where(status: 'approved').count
-      @total_ongoing_cash_advances = CashAdvRequest.where(status: 'ongoing').count
-      @total_cash_advances_year = CashAdvRequest.where("created_at >= ?", Time.current.beginning_of_year).sum(:amount)
-      @due_next_payroll = CashAdvRequest.where("cut_off <= ?", Time.current.end_of_month)
-    end
+  def index
+    @cash_adv_request = current_user.cash_adv_requests.first
+    @repayment_schedules = @cash_adv_request.repayment_schedules
+    @total_pending_requests = CashAdvRequest.where(status: 'pending').count
+    @total_released_requests = CashAdvRequest.where(status: 'released').count
+    @total_ongoing_cash_advances = CashAdvRequest.where(status: 'ongoing').count
+    @total_cash_advances_year = CashAdvRequest.where("created_at >= ?", Time.current.beginning_of_year).sum(:amount)
+    @total_ongoing_users = RepaymentSchedule.where(status: 'pending')
+                                            .distinct
+                                            .count(:cash_adv_request_id)
+   @pagy, @due_next_payroll = pagy( RepaymentSchedule
+                              .includes(cash_adv_request: :employee)
+                              .where("DAY(due_date) IN (15, 30)")
+                              .where("due_date <= ?", Time.current.end_of_month)
+                              .order(:due_date),items: 10
+                              )
   end
-  
+end
+
+
