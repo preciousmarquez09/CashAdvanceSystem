1b5ef59cf67ef5b923d0ca5492749dde18025b22
JohnPaul <john.estanislao@adish.com.ph>
2025-04-13 14:41:08 +0000

    feat(admin): add audit logs page with PDF export and add audit logs table in dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,11 +1,16 @@
 class Admin::DashboardController < ApplicationController
+  include Pagy::Backend
+  before_action :authenticate_user!
+
   
-    def index
-      @pending_requests = CashAdvRequest.where(status: "pending").count
-      @approved_requests = CashAdvRequest.where(status: "approved").count
-      @ongoing_cash_advances = CashAdvRequest.where(status: "ongoing").count
-      @total_cash_advances = CashAdvRequest.where("created_at >= ?", Time.current.beginning_of_year).sum(:amount)
-      @audit_logs = AuditLog.order(created_at: :desc).limit(10)
-    end
+  def index
+    @pending_requests = CashAdvRequest.where(status: "pending").count
+    @approved_requests = CashAdvRequest.where(status: "approved").count
+    @ongoing_cash_advances = CashAdvRequest.where(status: "ongoing").count
+    @total_cash_advances = CashAdvRequest.where("created_at >= ?", Time.current.beginning_of_year).sum(:amount)
+
+   
+    @q = AuditLog.ransack(params[:q])
+    @pagy_audit_logs, @audit_logs = pagy(@q.result.order(created_at: :desc), items: 10)
   end
-  
+end
