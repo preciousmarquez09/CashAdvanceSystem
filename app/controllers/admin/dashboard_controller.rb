b37a9210e0dfe243990025ddc6fd01e7da26a1f0
JohnPaul <john.estanislao@adish.com.ph>
2025-04-30 01:31:53 +0000

    fix(admin): remove def_authorize_admin authorization

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,7 @@ class Admin::DashboardController < ApplicationController
   include Pagy::Backend
   include RestrictPages
   
-  before_action :authorize_admin!
+
   before_action :authenticate_user!
   
   def index
